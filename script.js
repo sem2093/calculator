659b3ce77c3e39eec4ab8f9a2e56e69157bab1da
Steven Miller <131418264+sem2093@users.noreply.github.com>
2023-10-06 05:27:40 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,3 @@
-function darkMode(){
-
-// Get a reference to the <html> tag
-var htmlTag = document.documentElement;
-
-// Add an attribute and value to the <html> tag
-htmlTag.setAttribute("data-bs-theme", "dark");
-}
-
-
-function lightMode(){
-
-// Get a reference to the <html> tag
-var htmlTag = document.documentElement;
-
-// Add an attribute and value to the <html> tag
-htmlTag.setAttribute("data-bs-theme", "light");
-}
-
 
         function appendToDisplay(value) {
             const display = document.getElementById('display');
