5a2488bf2bb57aaf0d2b9fd447c075daac1870ab
Steven Miller <131418264+sem2093@users.noreply.github.com>
2023-10-06 04:41:44 -0600

    Update script.js

--- script.js
+++ script.js
@@ -16,3 +16,24 @@ var htmlTag = document.documentElement;
 // Add an attribute and value to the <html> tag
 htmlTag.setAttribute("data-bs-theme", "light");
 }
+
+
+        function appendToDisplay(value) {
+            const display = document.getElementById('display');
+            display.value += value;
+        }
+
+        function clearDisplay() {
+            const display = document.getElementById('display');
+            display.value = '';
+        }
+
+        function calculate() {
+            const display = document.getElementById('display');
+            try {
+                display.value = eval(display.value);
+            } catch (error) {
+                display.value = 'Error';
+            }
+        }
+    
